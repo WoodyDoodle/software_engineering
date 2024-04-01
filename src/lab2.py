454e5368ba30e0b698ef3a1261d64f5baab636c0
Dmitry Krutikov <krutikow24@gmail.com>
2024-04-01 20:58:42 +0500

    код к с.р.

---
+++ src/lab2.py
@@ -0,0 +1,11 @@
+def fib(n):
+    a, b = 1, 1
+    count = 0
+    with open('src/fib.txt', 'w') as txt:
+        while count < n:
+            yield a
+            a, b = b, a + b
+            txt.write(str(a) + '\n')
+            count += 1
+
+print(*[i for i in fib(200)])
