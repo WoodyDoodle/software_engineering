99460623183184b0dbd7d05d879e36684163fe32
Dmitry Krutikov <krutikow24@gmail.com>
2024-04-01 20:35:16 +0500

    код к с.р.

---
+++ src/lab3.py
@@ -0,0 +1,10 @@
+def sum_num():
+    try:
+        num = float(input('Введите число:\n'))
+        res = num + 2
+        return res
+    except ValueError:
+        return 'Неподходящий тип данных. Ожидалось число.'
+
+print(sum_num())
+print(sum_num())
